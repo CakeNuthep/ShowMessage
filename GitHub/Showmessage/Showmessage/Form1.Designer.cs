465a21fedb5991624c2fbafd11ca9317fa1d1d40
cake <cake.naruto_@hotmail.com>
2019-05-19 21:39:33 +0700

    first commit

---
+++ GitHub/Showmessage/Showmessage/Form1.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace Showmessage
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_message = new System.Windows.Forms.Label();
+            this.textBox_input = new System.Windows.Forms.TextBox();
+            this.button_ok = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label_message
+            // 
+            this.label_message.AutoSize = true;
+            this.label_message.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label_message.Location = new System.Drawing.Point(47, 23);
+            this.label_message.Name = "label_message";
+            this.label_message.Size = new System.Drawing.Size(182, 55);
+            this.label_message.TabIndex = 0;
+            this.label_message.Text = "HELLO";
+            // 
+            // textBox_input
+            // 
+            this.textBox_input.Location = new System.Drawing.Point(79, 103);
+            this.textBox_input.Name = "textBox_input";
+            this.textBox_input.Size = new System.Drawing.Size(100, 20);
+            this.textBox_input.TabIndex = 1;
+            // 
+            // button_ok
+            // 
+            this.button_ok.Location = new System.Drawing.Point(91, 146);
+            this.button_ok.Name = "button_ok";
+            this.button_ok.Size = new System.Drawing.Size(75, 23);
+            this.button_ok.TabIndex = 2;
+            this.button_ok.Text = "OK";
+            this.button_ok.UseVisualStyleBackColor = true;
+            this.button_ok.Click += new System.EventHandler(this.Button_ok_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 209);
+            this.Controls.Add(this.button_ok);
+            this.Controls.Add(this.textBox_input);
+            this.Controls.Add(this.label_message);
+            this.Name = "Form1";
+            this.Text = "Show Message";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_message;
+        private System.Windows.Forms.TextBox textBox_input;
+        private System.Windows.Forms.Button button_ok;
+    }
+}
+
